46dc7e961a889667f772c42395ae6eed5546a5e9
Doug Moscrop <doug.moscrop@gmail.com>
2015-06-30 10:55:35 -0400

    Preserve function arity

--- index.js
+++ index.js
@@ -2,28 +2,52 @@ var asyncWaterfall = require('async-waterfall'),
     ensureArray = require('ensure-array');
 
 module.exports = function () {
-	var middleware = ensureArray.apply(null, arguments);
+  var middleware = ensureArray.apply(null, arguments);
 
-	function async(req, res, next) {
-		var context = this;
+  function async(req, res, next) {
+    var startValues = Array.prototype.slice.call(arguments, 3);
+    var context = this;
 
-		function toAsync(target) {
-			return function() {
-				var callback = arguments[arguments.length - 1];
-				var values = Array.prototype.slice.call(arguments, 0, -1);
+    function toAsync(target) {
+      return function() {
+        var callback = arguments[arguments.length - 1];
+        var values = Array.prototype.slice.call(arguments, 0, -1);
 
-				target.apply(context, [req, res, callback].concat(values));
-			};
-		}
+        target.apply(context, [req, res, callback].concat(values));
+      };
+    }
 
-		asyncWaterfall(middleware.map(toAsync), function (err) {
-			if (err) {
-				next(err);
-			} else {
-				next();
-			}
-		});
-	}
+    var asyncMiddleware = middleware.map(toAsync);
 
-	return async;
+    function start(callback) {
+      callback.apply(callback, [null].concat(startValues));
+    }
+
+    asyncWaterfall([start].concat(asyncMiddleware), function (err) {
+      if (err) {
+        next(err);
+      } else {
+        next();
+      }
+    });
+  }
+
+  var length = 3;
+
+  if (middleware[0]) {
+    length = middleware[0].length;
+  }
+
+  switch(length) {
+    case 4:
+      return function(req, res, next, value) {
+        return async(req, res, next, value);
+      };
+    case 5:
+      return function(req, res, next, value, name) {
+        return async(req, res, next, value, name);
+      };
+    default:
+      return async;
+  }
 };
--- spec.js
+++ spec.js
@@ -5,132 +5,152 @@ var error = new Error();
 var counter = 0;
 
 function throwIfCalled() {
-	throw 'should not be called';
+  throw 'should not be called';
 }
 
 function many(howMany, middleware, done) {
-	var arr = [],
-	i;
+  var arr = [],
+  i;
 
-	for (i = 0; i < howMany; ++i) {
-		arr.push(middleware);
-	}
+  for (i = 0; i < howMany; ++i) {
+    arr.push(middleware);
+  }
 
-	serial(arr)({ foo: '123' }, {}, function (err) {
-		assert(typeof err === 'undefined');
-		assert.equal(counter, arr.length);
-		done();
-	}, 'value');
+  serial(arr)({ foo: '123' }, {}, function (err) {
+    assert(typeof err === 'undefined');
+    assert.equal(counter, arr.length);
+    done();
+  }, 'value');
 }
 
 beforeEach(function () {
-	counter = 0;
+  counter = 0;
 });
 
 describe('serial-middleware', function () {
 
-	function success(req, res, next) {
-		counter++;
-		next();
-	}
+  function success(req, res, next) {
+    counter++;
+    next();
+  }
 
-	function failure(req, res, next) {
-		counter++;
-		next(error);
-	}
+  function failure(req, res, next) {
+    counter++;
+    next(error);
+  }
 
-	it('calls next', function (done) {
-		serial(success)({}, {}, function next(err) {
-			assert(typeof err === 'undefined');
-			assert.equal(counter, 1);
-			done();
-		});
-	});
+  it('calls next', function (done) {
+    serial(success)({}, {}, function next(err) {
+      assert(typeof err === 'undefined');
+      assert.equal(counter, 1);
+      done();
+    });
+  });
 
-	it('passes error through', function (done) {
-		serial(failure)({}, {}, function next(err) {
-			assert.equal(err, error);
-			assert.equal(counter, 1);
-			done();
-		});
-	});
+  it('passes error through', function (done) {
+    serial(failure)({}, {}, function next(err) {
+      assert.equal(err, error);
+      assert.equal(counter, 1);
+      done();
+    });
+  });
 
-	it('works with several', function (done) {
-		many(5, success, done);
-	});
+  it('works with several', function (done) {
+    many(5, success, done);
+  });
 
-	it('works with a lot', function (done) {
-		many(1024, success, done);
-	});
+  it('works with a lot', function (done) {
+    many(1024, success, done);
+  });
 
-	it('short circuits when an error is provided', function (done) {
-		serial(success, success, failure, success, throwIfCalled)({}, {}, function (err) {
-			assert.equal(err, error);
-			assert.equal(counter, 3);
-			done();
-		});
-	});
+  it('short circuits when an error is provided', function (done) {
+    serial(success, success, failure, success, throwIfCalled)({}, {}, function (err) {
+      assert.equal(err, error);
+      assert.equal(counter, 3);
+      done();
+    });
+  });
 
-	describe('passing additional values', function() {
-		it('works with one', function (done) {
-			serial(
+  it('supports functions of arity 4', function(done) {
+    var param = serial(function(req, res, next, id) {
+      assert.equal(id, 'id-param');
+
+      counter++;
+      next(null, 'otherValue');
+    }, function(req, res, next, otherValue) {
+      assert.equal(otherValue, 'otherValue');
+      counter++;
+      next();
+    });
+
+    assert.equal(param.length, 4);
+
+    param({}, {}, function() {
+      assert.equal(counter, 2);
+      done();
+    }, 'id-param');
+  });
+
+  describe('passing additional values', function() {
+    it('works with one', function (done) {
+      serial(
 				function (req, res, next) {
-					counter++;
-					next(null, 'value');
+  counter++;
+  next(null, 'value');
 				},
 				function (req, res, next, value) {
-					counter++;
-					assert.equal(value, 'value');
-					next();
+  counter++;
+  assert.equal(value, 'value');
+  next();
 				}
 			)({}, {}, function (err) {
-				assert(typeof err === 'undefined');
-				assert.equal(counter, 2);
-				done();
+  assert(typeof err === 'undefined');
+  assert.equal(counter, 2);
+  done();
 			});
-		});
+    });
 
-		it('works with many', function (done) {
-			serial(
+    it('works with many', function (done) {
+      serial(
 				function (req, res, next) {
-					counter++;
-					next(null, 'value', 'otherValue');
+  counter++;
+  next(null, 'value', 'otherValue');
 				},
 				function (req, res, next, value, otherValue) {
-					counter++;
-					assert.equal(value, 'value');
-					assert.equal(otherValue, 'otherValue');
-					next();
+  counter++;
+  assert.equal(value, 'value');
+  assert.equal(otherValue, 'otherValue');
+  next();
 				}
 			)({}, {}, function (err) {
-				assert(typeof err === 'undefined');
-				assert.equal(counter, 2);
-				done();
+  assert(typeof err === 'undefined');
+  assert.equal(counter, 2);
+  done();
 			});
-		});
+    });
 
-		it('does not interfere with errors', function (done) {
-			serial(
+    it('does not interfere with errors', function (done) {
+      serial(
 				function (req, res, next) {
-					counter++;
-					next(null, 'value');
+  counter++;
+  next(null, 'value');
 				},
 				function (req, res, next, value) {
-					counter++;
-					assert.equal(value, 'value');
-					next();
+  counter++;
+  assert.equal(value, 'value');
+  next();
 				},
 				function(req, res, next) {
-					counter++;
-					next(error);
+  counter++;
+  next(error);
 				},
 				throwIfCalled
 			)({}, {}, function (err) {
-				assert(error === err);
-				assert.equal(counter, 3);
-				done();
+  assert(error === err);
+  assert.equal(counter, 3);
+  done();
 			});
-		});
+    });
 
-	});
+  });
 });
